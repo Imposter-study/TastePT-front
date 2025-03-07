80c044af17aabe0c3a8e444a4e0ec31c298f1509
yeonjae <pyj3740@gmail.com>
2025-03-07 16:59:24 +0900

    💄 Style change input type
    - input type password로 지정

--- src/routes/Changepassword.jsx
+++ src/routes/Changepassword.jsx
@@ -6,9 +6,17 @@ function Changepassword() {
     <div className="flex justify-center items-center min-h-screen pt-20">
       <div className="border-2 rounded-md border-gray-200 min-w-[300px] p-5">
         <div>
-          <Input inputLabel="Password" isrequired={true} />
-          <Input inputLabel="New Password" isrequired={true} />
-          <Input inputLabel="Password Confirm" isrequired={true} />
+          <Input inputLabel="Password" isrequired={true} inputType="password" />
+          <Input
+            inputLabel="New Password"
+            isrequired={true}
+            inputType="password"
+          />
+          <Input
+            inputLabel="Password Confirm"
+            isrequired={true}
+            inputType="password"
+          />
         </div>
         <div className="py-5">
           <Button buttonName="Change Password" />
