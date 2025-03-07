5383f746c5e6cb322539e89d819387d9beb4a009
yeonjae <pyj3740@gmail.com>
2025-03-07 14:27:33 +0900

    ✨ Feat : render change password page
    - 패스워드 변경 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
@@ -8,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Signup from "./routes/Signup";
 import Signin from "./routes/Signin";
 import Mypage from "./routes/Mypage";
+import Changepassword from "./routes/Changepassword";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +19,7 @@ function App() {
         <Route path="signup/" element={<Signup />} />
         <Route path="signin/" element={<Signin />} />
         <Route path="mypage/" element={<Mypage />} />
+        <Route path="password/" element={<Changepassword />} />
       </Routes>
     </div>
   );
