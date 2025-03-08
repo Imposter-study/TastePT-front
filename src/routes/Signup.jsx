c89207d0a640db9b455b429e56f3790e6254eb42
yeonjae <pyj3740@gmail.com>
2025-03-08 14:31:05 +0900

    ✨ Feat : signup with required field
    - 회원가입(필수 필드만 적용)

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,11 +1,39 @@
+import axios from "axios";
 import Button from "../components/Button";
 import Dropdown from "../components/Dropdown";
 import Input from "../components/Input";
 
 function Signup() {
+  const baseURL = import.meta.env.VITE_BASE_URL;
+
   const onSubmit = (event) => {
     event.preventDefault();
     console.log(event.target);
+    const formData = new FormData();
+    const signUpForm = event.target;
+
+    // console.log(signUpForm["email-input"].value);
+
+    formData.append("email", signUpForm["email-input"].value); // 이메일
+    formData.append("password", signUpForm["password-input"].value); // 패스워드
+    // 확인 패스워드
+    formData.append(
+      "password_confirm",
+      signUpForm["confirmpassword-input"].value
+    );
+    formData.append("nickname", signUpForm["nickname-input"].value); // 닉네임
+
+    console.log(formData);
+
+    axios
+      .post(`${baseURL}accounts/signup/`, formData)
+      .then((response) => console.log(response))
+      .catch((error) => {
+        const errorData = error.response.data;
+        console.log(errorData);
+        const firstKey = Object.keys(errorData)[0]; // errorData(json)의 첫 번째 key값
+        alert(error.response.data[firstKey]);
+      });
   };
 
   return (
