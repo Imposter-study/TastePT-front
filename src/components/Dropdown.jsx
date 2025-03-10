b00f2d12f9bd3ed5b8d3b43d2709c00073178351
yeonjae <pyj3740@gmail.com>
2025-03-10 17:08:52 +0900

    ✨ Feat : request signup API
    - 회원가입API로 요청

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,9 +1,16 @@
 function Dropdown({ dropdownLabel, options }) {
+  // 특수문자 및 공백 제거 후 id 생성
+  const optionID =
+    dropdownLabel.replace(/[^a-zA-Z0-9]/g, "").toLowerCase() + "-input";
+
   return (
     <div>
       <div className="select flex flex-col pt-4">
-        <label>{dropdownLabel}</label>
-        <select className="bg-gray-100 border border-gray-300 p-1 pl-3 my-2 rounded-md">
+        <label htmlFor={optionID}>{dropdownLabel}</label>
+        <select
+          id={optionID}
+          className="bg-gray-100 border border-gray-300 p-1 pl-3 my-2 rounded-md"
+        >
           <option value="" selected disabled hidden>
             {dropdownLabel}
           </option>
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -2,13 +2,34 @@ import axios from "axios";
 import Button from "../components/Button";
 import Dropdown from "../components/Dropdown";
 import Input from "../components/Input";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const baseURL = import.meta.env.VITE_BASE_URL;
+  const navigate = useNavigate();
 
-  const onSubmit = (event) => {
+  // console.log("cookie :", document.cookie);
+
+  // axios
+  //   .get(`${baseURL}accounts/csrf/`, { withCredentials: true })
+  //   .then((response) => console.log("CSRF Token:", response.data.csrfToken))
+  //   .catch((error) => console.error("CSRF Token 가져오기 실패", error));
+
+  const getCSRFToken = async () => {
+    try {
+      const response = await axios.post(`${baseURL}accounts/csrf/`, {
+        withCredentials: true,
+      });
+      return response.data.csrfToken;
+    } catch (error) {
+      console.error("CSRF Token 가져오기 실패", error);
+      return null;
+    }
+  };
+
+  const onSubmit = async (event) => {
     event.preventDefault();
-    console.log(event.target);
+    // console.log(event.target);
     const formData = new FormData();
     const signUpForm = event.target;
 
@@ -23,12 +44,62 @@ function Signup() {
     );
     formData.append("nickname", signUpForm["nickname-input"].value); // 닉네임
 
-    console.log(formData);
+    // 나이
+    const age = signUpForm["age-input"].value;
+    if (age) {
+      formData.append("age", age);
+    }
+
+    // 성별
+    console.log(signUpForm["gender-input"].value);
+    const gender = signUpForm["gender-input"].value;
+    if (gender === "남자") {
+      formData.append("gender", "M");
+    } else if (gender === "여자") {
+      formData.append("gender", "F");
+    }
+
+    // 알러지
+    // 추가 예정
+
+    // 선호 요리
+    // 추가 예정
+
+    // 다이어트 여부 (백엔드 모델에 없음)
+    const isDiet = signUpForm["diet-input"].value;
+    if (isDiet === "유") {
+      console.log(isDiet);
+      // formData.append("diet", "Y");
+    } else if (isDiet === "무") {
+      console.log(isDiet);
+      // formData.append("diet", "N");
+    }
+
+    const userData = {
+      email: signUpForm["email-input"].value,
+      password: signUpForm["password-input"].value,
+      password_confirm: signUpForm["confirmpassword-input"].value,
+      nickname: signUpForm["nickname-input"].value,
+    };
+
+    const csrfToken = await getCSRFToken(); // ✅ CSRF 토큰을 먼저 가져옴
+    console.log("CSRF TOKEN : ", csrfToken);
 
     axios
-      .post(`${baseURL}accounts/signup/`, formData)
-      .then((response) => console.log(response))
+      .post(`${baseURL}accounts/`, formData, {
+        withCredentials: true,
+        headers: {
+          "X-CSRFToken": csrfToken, // ✅ CSRF 토큰을 요청 헤더에 포함
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        alert("회원가입이 완료되었습니다.");
+        navigate("/");
+      })
       .catch((error) => {
+        console.log();
+        console.log(error);
         const errorData = error.response.data;
         console.log(errorData);
         const firstKey = Object.keys(errorData)[0]; // errorData(json)의 첫 번째 key값
@@ -62,10 +133,7 @@ function Signup() {
                 <Input inputLabel="Age" isrequired={false} inputType="number" />
               </div>
               <div className="flex-1 p-5 min-w-[300px] w-full">
-                <Dropdown
-                  dropdownLabel="Gender"
-                  options={["Male", "Female", "Other"]}
-                />
+                <Dropdown dropdownLabel="Gender" options={["남자", "여자"]} />
                 <Dropdown
                   dropdownLabel="Allerge"
                   options={["알러지1", "알러지2", "Other"]}
