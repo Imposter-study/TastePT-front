033c1ab104d9ee62fec9fde2f2332bf25d83ee0b
yeonjae <pyj3740@gmail.com>
2025-03-12 17:56:58 +0900

    ✨ Feat : add button click event
    - 버튼 클릭 이벤트 추가
    - 로고 버튼 클릭 시 메인페이지로 이동
    - Sign in 버튼 -> 로그인 페이지
    - Sign up 버튼 -> 회원가입 페이지

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
+  const goSignUp = () => {
+    navigate("/signup");
+  };
+
+  const goSignIn = () => {
+    navigate("/signin");
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-screen min-w-[100px] overflow-x-auto bg-white">
+    <div className="fixed top-0 left-0 w-screen min-w-[100px] overflow-x-auto bg-white z-50">
       <div className="container mx-auto flex justify-between items-center border-b-2 border-gray-300 p-5">
-        <div className="m-1 font-bold text-lg">logo</div>
-        <div className="flex">
+        <div className="m-1 font-bold text-lg cursor-pointer" onClick={goHome}>
+          logo
+        </div>
+        <div className="flex items-center">
           <div className="px-1">
             <Button
               buttonName="Sign up"
               bgColor="gray"
               textColor="black"
               borderColor="gray"
+              onClick={goSignUp}
             />
           </div>
           <div className="px-1">
-            <Button buttonName="Sign in" />
+            <Button buttonName="Sign in" onClick={goSignIn} />
           </div>
         </div>
       </div>
