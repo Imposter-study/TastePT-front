7f7880453f167f840985878dbb6c8f3866588523
yeonjae <pyj3740@gmail.com>
2025-03-13 10:22:23 +0900

    ✨ Feat : create post creation page
    - 게시글 생성 페이지 생성
    - community/new/ 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Navbar from "./components/Navbar";
-import Signup from "./routes/Signup";
-import Signin from "./routes/Signin";
-import Mypage from "./routes/Mypage";
+import Signup from "./routes/accounts/Signup";
+import Signin from "./routes/accounts/Signin";
+import Mypage from "./routes/accounts/Mypage";
+import CreatePost from "./routes/community/CreatePost";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +19,7 @@ function App() {
         <Route path="signup/" element={<Signup />} />
         <Route path="signin/" element={<Signin />} />
         <Route path="mypage/" element={<Mypage />} />
+        <Route path="community/new/" element={<CreatePost />} />
       </Routes>
     </div>
   );
