71f54c661a07b3057d3b67aa2a124a0e55ce5a7e
yeonjae <pyj3740@gmail.com>
2025-03-13 17:29:55 +0900

    ✨ Feat : add post button, default thumbnail
    - 게시글 작성 버튼 생성
    - default thumbnail 이미지 생성
    - content의 text만 가져와서 렌더링

--- src/routes/community/PostList.jsx
+++ src/routes/community/PostList.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import defaultImage from "/no-image.png";
 
 function PostList() {
   const apiURL = import.meta.env.VITE_API_URL;
@@ -34,22 +36,38 @@ function PostList() {
       {loading ? (
         "Loading..."
       ) : (
-        <div>
-          <h1>PostList</h1>
-          <div className="h-screen">
+        <div className="w-5/6 px-10">
+          {/* <h1>PostList</h1> */}
+          <div className="flex justify-end">
+            <div className="min-w-[170px] pt-3">
+              <Link to="/community/new">
+                <Button buttonName="게시글 작성하러 가기" />
+              </Link>
+            </div>
+          </div>
+          <div className="">
             {postList.map((post) => (
-              <Link to={`/community/${post.id}`}>
-                <div className="border p-5 my-5">
-                  <h2>{post.title}</h2>
-                  {/* <div className="text-gray-400"
+              <Link to={`/community/${post.id}`} key={post.id}>
+                <div className="flex my-5">
+                  <div className="m-1 p-1">
+                    <img
+                      src={defaultImage}
+                      alt="thumbnail"
+                      className="size-15"
+                    />
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <h2 className="font-bold text-xl">{post.title}</h2>
+                    {/* <div className="text-gray-400"
                   dangerouslySetInnerHTML={{
                     __html: post.content
                       .replace(/src="\/media\//g, `src="${baseURL}media/`)
                       .slice(0, 100),
                   }}
                 /> */}
-                  <div className="max-w-[800px] text-gray-400">
-                    {extractString(post.content)}
+                    <div className="max-w-[800px] text-gray-400">
+                      {extractString(post.content).slice(0, 100)}...
+                    </div>
                   </div>
                 </div>
               </Link>
