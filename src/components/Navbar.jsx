140934c1fbe6547b3c8cc9a014b192ff9db2c6d8
yeonjae <pyj3740@gmail.com>
2025-03-13 17:06:56 +0900

    💄 Style : set minimum width
    - 네브바 최소 길이 고정 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 function Navbar() {
   return (
-    <div className="fixed top-0 left-0 w-screen min-w-[100px] overflow-x-auto bg-white z-50">
+    <div className="fixed top-0 left-0 w-screen min-w-[400px] overflow-x-auto bg-white z-50">
       <div className="container mx-auto flex justify-between items-center border-b-2 border-gray-300 p-5">
         <Link to="/">
           <div className="m-1 font-bold text-lg cursor-pointer">logo</div>
