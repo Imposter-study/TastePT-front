9a5cd3b9de7b81427f1428fb1fd06d87d2d86eb6
yeonjae <pyj3740@gmail.com>
2025-03-17 14:39:15 +0900

    ✨ Feat : add random nickname
    - 랜덤 닉네임 생성 API 연동
    - 주사위 버튼 클릭 시 랜덤 닉네임 자동 입력

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,8 @@
-function Input({ inputLabel, isrequired, inputType }) {
-    // 특수문자 및 공백 제거 후 id 생성
-  const inputID = inputLabel.replace(/[^a-zA-Z0-9]/g, '').toLowerCase() + "-input";
-//   console.log(inputID);
+function Input({ inputLabel, isrequired, inputType, value, onChange }) {
+  // 특수문자 및 공백 제거 후 id 생성
+  const inputID =
+    inputLabel.replace(/[^a-zA-Z0-9]/g, "").toLowerCase() + "-input";
+  //   console.log(inputID);
 
   return (
     <div className="flex flex-col pt-3">
@@ -12,6 +13,8 @@ function Input({ inputLabel, isrequired, inputType }) {
         placeholder={inputLabel}
         required={isrequired}
         type={inputType}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
--- src/routes/accounts/Signup.jsx
+++ src/routes/accounts/Signup.jsx
@@ -8,6 +8,8 @@ import { publicAccountAPI } from "../../api/accountApi";
 
 function Signup() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [randomNickname, setRandomNickname] = useState("");
   const [allergyList, setAllergyList] = useState([]);
   const [preferredCuisineList, setPreferredCuisineList] = useState([]);
   const [selectedAllergyList, setSelectedAllergyList] = useState([]);
@@ -31,6 +33,19 @@ function Signup() {
         (preferredCuisine) => preferredCuisine.cuisine
       );
       setPreferredCuisineList(preferredCuisines);
+      setLoading(false);
+    });
+  };
+
+  // 랜덤 닉네임 생성
+  const generateRandomNickname = () => {
+    publicAccountAPI.get("random_nickname/").then((response) => {
+      // console.log(response);
+      // console.log(response.data);
+      const nickname = response.data.nickname;
+      console.log(nickname);
+      setRandomNickname(nickname);
+      // setLoading(false);
     });
   };
 
@@ -115,75 +130,101 @@ function Signup() {
   useEffect(() => {
     getAllergyList();
     getPreferredCuisineList();
+    // generateRandomNickname();
   }, []);
 
   return (
     <div className="flex justify-center items-center min-h-screen pt-20">
-      <div className="border-2 rounded-md w-fit border-gray-300 m-5">
-        <form id="signUpForm" onSubmit={onSubmit}>
-          <div className="flex">
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div className="border-2 rounded-md w-fit border-gray-300 m-5">
+          <form id="signUpForm" onSubmit={onSubmit}>
             <div className="flex">
-              {/* 왼쪽 컨테이너 */}
-              <div className="flex-1 p-5 min-w-[300px] w-full">
-                {/* 이메일 */}
-                <Input
-                  inputLabel="Email *"
-                  isrequired={true}
-                  inputType="email"
-                />
+              <div className="flex">
+                {/* 왼쪽 컨테이너 */}
+                <div className="flex-1 p-5 min-w-[300px] w-full">
+                  {/* 이메일 */}
+                  <Input
+                    inputLabel="Email *"
+                    isrequired={true}
+                    inputType="email"
+                  />
 
-                {/* 패스워드 */}
-                <Input
-                  inputLabel="Password *"
-                  isrequired={true}
-                  inputType="password"
-                />
+                  {/* 패스워드 */}
+                  <Input
+                    inputLabel="Password *"
+                    isrequired={true}
+                    inputType="password"
+                  />
 
-                {/* 확인 패스워드 */}
-                <Input
-                  inputLabel="Confirm Password *"
-                  isrequired={true}
-                  inputType="password"
-                />
+                  {/* 확인 패스워드 */}
+                  <Input
+                    inputLabel="Confirm Password *"
+                    isrequired={true}
+                    inputType="password"
+                  />
 
-                {/* 닉네임 */}
-                <Input inputLabel="Nickname *" isrequired={true} />
+                  {/* 닉네임 */}
+                  <div className="flex items-end gap-2 w-full">
+                    <div className="flex-1">
+                      <Input
+                        inputLabel="Nickname *"
+                        isrequired={true}
+                        value={randomNickname}
+                        onChange={(e) => setRandomNickname(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <div
+                        className="p-1 px-4 border border-gray-400 rounded-md min-w-max max-w-full cursor-pointer"
+                        onClick={generateRandomNickname}
+                      >
+                        🎲
+                      </div>
+                    </div>
+                  </div>
 
-                {/* 나이 */}
-                <Input inputLabel="Age" isrequired={false} inputType="number" />
-              </div>
+                  {/* 나이 */}
+                  <Input
+                    inputLabel="Age"
+                    isrequired={false}
+                    inputType="number"
+                  />
+                </div>
 
-              {/* 오른쪽 컨테이너 */}
-              <div className="flex-1 p-5 min-w-[300px] w-full">
-                {/* 성별 */}
-                <Dropdown dropdownLabel="Gender" options={["남자", "여자"]} />
+                {/* 오른쪽 컨테이너 */}
+                <div className="flex-1 p-5 min-w-[300px] w-full">
+                  {/* 성별 */}
+                  <Dropdown dropdownLabel="Gender" options={["남자", "여자"]} />
 
-                {/* 다이어트 여부 */}
-                <Dropdown dropdownLabel="Diet" options={["True", "False"]} />
+                  {/* 다이어트 여부 */}
+                  <Dropdown dropdownLabel="Diet" options={["True", "False"]} />
 
-                {/* 알러지 */}
-                <CheckBox
-                  boxTitle="Allergy"
-                  componentList={allergyList}
-                  selectedData={setSelectedAllergyList}
-                />
+                  {/* 알러지 */}
+                  <CheckBox
+                    boxTitle="Allergy"
+                    componentList={allergyList}
+                    selectedData={setSelectedAllergyList}
+                  />
 
-                {/* 선호 요리 */}
-                <CheckBox
-                  boxTitle="Preferred cuisine"
-                  componentList={preferredCuisineList}
-                  selectedData={setSelectedPreferredCuisineList}
-                />
+                  {/* 선호 요리 */}
+                  <CheckBox
+                    boxTitle="Preferred cuisine"
+                    componentList={preferredCuisineList}
+                    selectedData={setSelectedPreferredCuisineList}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex justify-center">
-            <div className="flex justify-center w-2/5 py-10">
-              <Button buttonName="Submit" />
+            <div className="flex justify-center">
+              <div className="flex justify-center w-2/5 py-10">
+                <Button buttonName="Submit" onSubmit={onSubmit} />
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
