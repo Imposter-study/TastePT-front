e8e730b2c66ff2e99883f8a24e23060fc91ab628
yeonjae <pyj3740@gmail.com>
2025-03-20 16:11:05 +0900

    ✨ Feat: create footer
    - 푸터 생성
    - 이메일, copyright, 노션, 깃허브 정보 제공

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,29 @@ import PostDetail from "./routes/community/PostDetail";
 import PostList from "./routes/community/PostList";
 import Profile from "./routes/accounts/Profile";
 import ChatBot from "./routes/ChatBot";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Navbar />
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="signup/" element={<Signup />} />
-        <Route path="signin/" element={<Signin />} />
-        <Route path="mypage/" element={<Mypage />} />
-        <Route path=":nickname/" element={<Profile />} />
-        <Route path="password/" element={<Changepassword />} />
-        <Route path="community/" element={<PostList />} />
-        <Route path="community/new/" element={<CreatePost />} />
-        <Route path="community/:postID/" element={<PostDetail />} />
-        <Route path="community/:postID/edit/" element={<EditPost />} />
-        <Route path="chatbot/" element={<ChatBot />} />
-      </Routes>
-    </div>
+    <>
+      <div className="container mx-auto">
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="signup/" element={<Signup />} />
+          <Route path="signin/" element={<Signin />} />
+          <Route path="mypage/" element={<Mypage />} />
+          <Route path=":nickname/" element={<Profile />} />
+          <Route path="password/" element={<Changepassword />} />
+          <Route path="community/" element={<PostList />} />
+          <Route path="community/new/" element={<CreatePost />} />
+          <Route path="community/:postID/" element={<PostDetail />} />
+          <Route path="community/:postID/edit/" element={<EditPost />} />
+          <Route path="chatbot/" element={<ChatBot />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import notionIcon from "../assets/notion-icon.webp";
+import githubIcon from "../assets/github-icon.png";
+import { useNavigate } from "react-router-dom";
+
+function Footer() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex justify-between items-center w-full bg-gray-100 ">
+      <div className="text-gray-500 text-xs p-5">
+        <p>Email : imposterstudy@gmail.com</p>
+        <p>Copyright ⓒ 2025. ImposterStudy All rights reserved.</p>
+      </div>
+      <div className="flex gap-3 p-5">
+        <a href="https://www.notion.so/P-T-1a87d4611a6f80938cc9e0d41d01ca40?pvs=4">
+          <img
+            src={notionIcon}
+            alt="notion-icon"
+            className="size-6 object-cover"
+          />
+        </a>
+        <a href="https://github.com/Imposter-study/TastePT">
+          <img
+            src={githubIcon}
+            alt="github-icon"
+            className="size-6 object-cover"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
