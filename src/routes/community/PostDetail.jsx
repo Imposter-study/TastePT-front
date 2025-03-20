289531206744e04762eb8882269e3386dfea8f5f
yeonjae <pyj3740@gmail.com>
2025-03-20 12:29:50 +0900

    ✨ Feat: Use ProtectedButton for login redirection
    - 비로그인 사용자의 권한 없는 접근 방지

--- src/routes/community/PostDetail.jsx
+++ src/routes/community/PostDetail.jsx
@@ -9,6 +9,7 @@ import {
 import { useRecoilValue } from "recoil";
 import { authUser } from "../../recoil/authAtom";
 import defaultProfile from "/image.png";
+import ProtectedButton from "../../components/ProtectedButton";
 
 function PostDetail() {
   const baseURL = import.meta.env.VITE_BASE_URL;
@@ -151,16 +152,22 @@ function PostDetail() {
           {/* 댓글 */}
           <div className="border-t-2 border-gray-300 min-w-[200px]">
             <div className="py-5 m-1">
-              <form id="comment-form" className="flex" onSubmit={submitComment}>
-                <input
-                  id="comment-input"
-                  className="border-2 border-gray-300 rounded-md w-full pl-3"
-                  placeholder="comment"
-                />
-                <div className="px-1">
-                  <Button buttonName="submit" />
-                </div>
-              </form>
+              <ProtectedButton to={`/community/${postID}/`}>
+                <form
+                  id="comment-form"
+                  className="flex"
+                  onSubmit={submitComment}
+                >
+                  <input
+                    id="comment-input"
+                    className="border-2 border-gray-300 rounded-md w-full pl-3"
+                    placeholder="comment"
+                  />
+                  <div className="px-1">
+                    <Button buttonName="submit" />
+                  </div>
+                </form>
+              </ProtectedButton>
             </div>
             {comments.map((comment) => (
               <Comment
