32ff3f1c09678bf582077b2d413fa77f04d7f9e5
yeonjae <pyj3740@gmail.com>
2025-03-20 11:05:00 +0900

    ✨ Feat: add protected router for auth
    - 미로그인 상태의 유저가 회원 전용 기능에 접근시 로그인 페이지로 리다이렉트

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,27 @@ import PostDetail from "./routes/community/PostDetail";
 import PostList from "./routes/community/PostList";
 import Profile from "./routes/accounts/Profile";
 import ChatBot from "./routes/ChatBot";
+import ProtectedRouter from "./routes/ProtectedRouter";
 
 function App() {
   return (
     <div className="container mx-auto">
       <Navbar />
       <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="signup/" element={<Signup />} />
-        <Route path="signin/" element={<Signin />} />
-        <Route path="mypage/" element={<Mypage />} />
-        <Route path=":nickname/" element={<Profile />} />
-        <Route path="password/" element={<Changepassword />} />
-        <Route path="community/" element={<PostList />} />
-        <Route path="community/new/" element={<CreatePost />} />
-        <Route path="community/:postID/" element={<PostDetail />} />
-        <Route path="community/:postID/edit/" element={<EditPost />} />
-        <Route path="chatbot/" element={<ChatBot />} />
+        <Route path="/" element={<Home />} />                                    {/* 메인페이지 */}
+        <Route path="signup/" element={<Signup />} />                            {/* 회원가입 페이지 */}
+        <Route path="signin/" element={<Signin />} />                            {/* 로그인 페이지 */} 
+        {/* 로그인 후 접근 가능한 페이지 */}
+        <Route element={<ProtectedRouter />}>
+          <Route path="community/new/" element={<CreatePost />} />               {/* 게시글 작성 페이지 */}
+          <Route path="community/:postID/edit/" element={<EditPost />} />        {/* 게시글 수정 페이지 */}
+          <Route path="mypage/" element={<Mypage />} />                          {/* 마이페이지(프로필 수정) */}
+          <Route path="password/" element={<Changepassword />} />                {/* 비밀번호 변경 페이지 */}
+          <Route path="chatbot/" element={<ChatBot />} />                        {/* 챗봇 페이지 */}
+          <Route path=":nickname/" element={<Profile />} />                      {/* 프로필 페이지 */}
+        </Route>
+        <Route path="community/" element={<PostList />} />                       {/* 게시글 목록 페이지 */}
+        <Route path="community/:postID/" element={<PostDetail />} />             {/* 게시글 상세 페이지 */}
       </Routes>
     </div>
   );
---
+++ src/routes/ProtectedRouter.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { isAuthenticated } from "../recoil/authAtom";
+
+const ProtectedRouter = () => {
+  const isAuth = useRecoilValue(isAuthenticated); // 로그인 여부 확인
+
+  // 로그인 안되어있으면 로그인 페이지로 리다이렉트
+  if (!isAuth) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRouter;
