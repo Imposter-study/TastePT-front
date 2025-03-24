deabd8ed39cc1504cea6132ddedefa2e84c37089
jeil <sjl01002@gmail.com>
2025-03-24 15:10:52 +0900

    🩹 Fix: image
    - 이미지 url 경로 수정

--- src/routes/accounts/Profile.jsx
+++ src/routes/accounts/Profile.jsx
@@ -8,6 +8,7 @@ import { publicAccountAPI } from "../../api/accountApi";
 import { useParams } from "react-router-dom";
 import CheckBox from "../../components/CheckBox";
 import { authUser } from "../../recoil/authAtom";
+import { getImageUrl } from "../../utils/imageUtils";
 
 function Profile() {
   const { nickname } = useParams();
@@ -35,11 +36,7 @@ function Profile() {
         <div className="w-fit border-gray-300 m-5">
           <div className="flex flex-col items-center max-w-[150px] pb-2 ">
             <img
-              src={
-                user.profile_picture
-                  ? user.profile_picture
-                  : defaultProfile
-              }
+              src={getImageUrl(user.profile_picture, defaultProfile)}
               alt="프로필 이미지"
               className="size-25 mb-3 rounded-full object-cover"
             />
