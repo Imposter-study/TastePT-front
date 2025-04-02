e5fee3feceb0ac7510d4c87947616ab9404217cf
yeonjae <pyj3740@gmail.com>
2025-04-02 11:39:59 +0900

    ⚡️ Feat: Implement compression using Brotli for improved performance
    압축 기능을 추가하여 번들 크기 최적

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    compression({ algorithm: "brotliCompress" }),
+  ],
   server: {
     proxy: {
-      '/api/v1': {
-        target: 'http://127.0.0.1:8000/',
+      "/api/v1": {
+        target: "http://127.0.0.1:8000/",
         changeOrigin: true,
-        rewrite: (path) => path
-      }
-    }
+        rewrite: (path) => path,
+      },
+    },
   },
   build: {
-    outDir: 'dist'
-  }
+    outDir: "dist",
+  },
 });
