e5ed4e54cd9e81c7f18b42b2ab7720bd52f172a5
yeonjae <pyj3740@gmail.com>
2025-04-08 17:12:46 +0900

    ✨ Feat: create custom hook
    - axios get 요청 커스텀 훅 생성

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export function useAxios(endpoint, axiosAPI) {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [notFound, setNotFound] = useState(false);
+
+  const get = () => {
+    axiosAPI
+      .get(`${endpoint}/`)
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        // console.log(error);
+        // console.log(error.response.status);
+        if (error.response.status === 404) {
+          setNotFound(true);
+        } else {
+          setNotFound(false);
+        }
+      });
+  };
+
+  useEffect(() => {
+    get();
+  }, [endpoint]);
+
+  return { data, loading, notFound };
+}
